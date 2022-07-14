395d7b5786c9e03adff22744ed052c306a315211
abeer6 <108021869+abeer6@users.noreply.github.com>
2022-07-14 05:59:05 +0300

    my homework2

---
+++ asstes/javescript.js
@@ -0,0 +1,33 @@
+//Get the button:
+//mybutton = document.getElementById("fa");
+
+// When the user scrolls down 20px from the top of the document, show the button
+//window.onscroll = function() {scrollFunction()};
+
+//function scrollFunction() {
+  //if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+   // mybutton.style.display = "block";
+  //} else {
+   // mybutton.style.display = "none";
+ // }
+//}
+
+// When the user clicks on the button, scroll to the top of the document
+//function topFunction() {
+ // document.body.scrollTop = 0; // For Safari
+  //document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+//}//
+subscribe= (نعم,لا);
+
+let subscribe = prompt("هل تريد الاشتراك في منصتي ؟رد بنعم اولا"    );
+subscribe= (نعم,لا);
+
+if (subscribe ==نعم) {
+  alert("مرحبا فيك في منصتي");  
+}
+
+
+
+else  (subscribe == لا){
+ alert("عدلنا اذا غيرت رايك" );
+}
